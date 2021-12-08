1844d72d28ea870cef5b24297b20a87f0a4a0296
Saijeet7 <np03s200062@heraldcollege.edu.np>
2021-12-08 05:52:29 +0545

    Detect Faces

--- face_detector.py
+++ face_detector.py
@@ -1,3 +1,16 @@
 import cv2
+#Load some pre-trained data on face frontals from opencv
+trained_face_data= cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+img = cv2.imread('RDJ.png')
+#Convert to grayscale
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#Detect Faces
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+
+
+cv2.imshow('Face detection',grayscaled_img)
+cv2.waitKey()
 print("Code completed")
